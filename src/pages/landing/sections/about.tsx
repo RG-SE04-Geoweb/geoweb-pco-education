e779392f7be24a63018297442b8d630b5746a5c4
jordang7 <77jgreenberg77@gmail.com>
2022-03-31 14:53:15 -0400

    tragedy of the commons image

--- src/pages/landing/sections/about.tsx
+++ src/pages/landing/sections/about.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Box, Image, Heading, HStack, Center } from "@chakra-ui/react";
-import placeholderImage from "../../../constants/images/placeholder-image.png";
+import tragedyOTC from "../../../constants/images/tragedyOfTheCommons.png";
 export const About = () => {
   return (
     <HStack spacing={3}>
-      <Box p={15} flex="1">
+      <Box p={15} pl={"50px"} flex="1">
         <Center>
-          <Image src={placeholderImage} />
+          <Image src={tragedyOTC} />
         </Center>
       </Box>
       <Box p={15} flex="1">
