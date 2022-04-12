4b808701963f320ce34c6d3e48be48c96ce10da1
jordang7 <77jgreenberg77@gmail.com>
2022-04-12 13:21:49 -0400

    switched to simplegrid for IRL cards

--- src/pages/landing/sections/pcoRealLife.tsx
+++ src/pages/landing/sections/pcoRealLife.tsx
@@ -1,43 +1,41 @@
 import React from "react";
-import { Box, Image, Heading, HStack, Flex, Text, VStack, Button, Link, } from "@chakra-ui/react";
+import { Box, Image, Heading, HStack, Text, Button, Link, SimpleGrid, Center } from "@chakra-ui/react";
 import { irlList } from "../../../constants/data/IRLData";
 import background from "../../../constants/images/background2.png"
 
 export const PcoRealLife = () => {
     return (
         <Box py={"100px"} backgroundImage={background} >
-            <Flex justifyContent={'center'} alignContent={'center'}>
-                <HStack spacing={3}>
-                    {irlList.map((project) => {
-                        return (
-                            <Box maxW='sm' height={'100%'} borderWidth='1px' borderRadius='lg' overflow='hidden' p={15} flex="1" bg={'whiteAlpha.300'} key={project.name} display={'flex'} flexDirection={'column'} >
-                                <VStack alignItems={'flex-start'}>
-                                    <HStack pb={"10px"}>
-                                        <Image src={project.image} boxSize='50px' />
-                                        <Heading as="h1" size={"lg"} > {project.name}
-                                        </Heading>
-                                    </HStack>
-                                    <Heading as="h2" size={"md"}  >WTF is it?</Heading>
-                                    <Text color={"whiteAlpha.900"} >
-                                        {project.wtfIsIt}
-                                    </Text>
-                                    <Heading as="h2" size={"md"}>Why does this matter?</Heading>
-                                    <Text color={"whiteAlpha.900"} pb={"40px"}>
-                                        {project.whyDoesThisMatter}
-                                    </Text>
+            <SimpleGrid minChildWidth='400px' spacing='40px' px={"40px"}>
+                {irlList.map((project) => {
+                    return (
+                        <Center>
+                            <Box maxW='sm' height={'100%'} borderWidth='1px' borderRadius='lg' overflow='hidden' p={15} bg={'whiteAlpha.300'} key={project.name} display={'flex'} flexDirection={'column'} >
+                                <HStack pb={"10px"}>
+                                    <Image src={project.image} boxSize='50px' />
+                                    <Heading as="h1" size={"lg"} > {project.name}
+                                    </Heading>
+                                </HStack>
+                                <Heading as="h2" size={"md"}  >WTF is it?</Heading>
+                                <Text color={"whiteAlpha.900"} >
+                                    {project.wtfIsIt}
+                                </Text>
+                                <Heading as="h2" size={"md"}>Why does this matter?</Heading>
+                                <Text color={"whiteAlpha.900"} pb={"40px"}>
+                                    {project.whyDoesThisMatter}
+                                </Text>
 
-                                </VStack>
                                 <Link href={project.link} target="_blank" marginTop={"auto"}>
                                     <Button>
                                         {project.name}
                                     </Button>
                                 </Link>
 
-                            </Box>)
-                    })}
+                            </Box>
+                        </Center>)
+                })}
 
-                </HStack>
-            </Flex >
+            </SimpleGrid>
         </Box >
     );
 };
